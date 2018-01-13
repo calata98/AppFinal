018ff539e488d0f3c9964b8248a36a6060361163
calata98 <pabloo-c-1998@hotmail.com>
2018-01-13 19:25:36 +0100

    Borrado Menu

--- app/src/main/java/calatayud/pablo/appfinal/MainActivity.java
+++ app/src/main/java/calatayud/pablo/appfinal/MainActivity.java
@@ -2,6 +2,9 @@ package calatayud.pablo.appfinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
