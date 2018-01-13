321d968926c062a0efe7fd1fc02d2e0600e805c8
calata98 <pabloo-c-1998@hotmail.com>
2018-01-13 19:56:39 +0100

    Añadida activity usuario

--- app/src/main/java/calatayud/pablo/appfinal/MainActivity.java
+++ app/src/main/java/calatayud/pablo/appfinal/MainActivity.java
@@ -1,10 +1,12 @@
 package calatayud.pablo.appfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void acceder(View view){
+        Intent intAcceder = new Intent(this, UsuarioActivity.class);
+        startActivity(intAcceder);
     }
 
 
