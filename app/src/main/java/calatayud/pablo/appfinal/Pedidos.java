a79fddf4691c8683c350d682c1ece8c8ac4582a4
calata98 <pabloo-c-1998@hotmail.com>
2018-01-14 01:04:49 +0100

    Todo entero

---
+++ app/src/main/java/calatayud/pablo/appfinal/Pedidos.java
@@ -0,0 +1,28 @@
+package calatayud.pablo.appfinal;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by calata98 on 13-Jan-18.
+ */
+
+public class Pedidos extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.menu_pedidos, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Profile");
+    }
+}
