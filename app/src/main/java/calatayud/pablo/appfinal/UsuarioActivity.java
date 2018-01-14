5370e926874de948e3aa2866e5812ba4aecf036f
calata98 <pabloo-c-1998@hotmail.com>
2018-01-14 12:11:41 +0100

    Añadidos los menus pero sin layouts

--- app/src/main/java/calatayud/pablo/appfinal/UsuarioActivity.java
+++ app/src/main/java/calatayud/pablo/appfinal/UsuarioActivity.java
@@ -14,11 +14,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class UsuarioActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static int cont = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +42,12 @@ public class UsuarioActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        Profile profileF = new Profile();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame,profileF)
+                .addToBackStack(null)
+                .commit();
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -50,8 +57,15 @@ public class UsuarioActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+            cont = 0;
         } else {
-            super.onBackPressed();
+            cont++;
+            if (cont >= 2){
+                this.finish();
+                cont = 0;
+            }else{
+                Toast.makeText(this, "Pulse otra vez el botón de atrás para salir", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -63,13 +77,29 @@ public class UsuarioActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_profile) {
-
+            Profile profileF = new Profile();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame,profileF)
+                    .addToBackStack(null)
+                    .commit();
         } else if (id == R.id.nav_pedido) {
-
+            Pedidos pedidosF = new Pedidos();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame,pedidosF)
+                    .addToBackStack(null)
+                    .commit();
         } else if (id == R.id.nav_admin) {
-
+            Admin adminF = new Admin();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, adminF)
+                    .addToBackStack(null)
+                    .commit();
         } else if (id == R.id.nav_tool) {
-
+            Opciones opcionesF = new Opciones();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, opcionesF)
+                    .addToBackStack(null)
+                    .commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
